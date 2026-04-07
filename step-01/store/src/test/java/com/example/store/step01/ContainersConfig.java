88a2a293579cfcb1d322f7f1cfe194e4763f8113
Laurent Broudoux <laurent.broudoux@gmail.com>
2026-04-07 14:59:29 +0200

    Adding mocks for Anthropic data

--- step-01/store/src/test/java/com/example/store/step01/ContainersConfig.java
+++ step-01/store/src/test/java/com/example/store/step01/ContainersConfig.java
@@ -1,9 +1,13 @@
 package com.example.store.step01;
 
+import io.github.microcks.testcontainers.MicrocksContainer;
+
 import org.springframework.boot.micrometer.metrics.autoconfigure.export.otlp.OtlpMetricsConnectionDetails;
 import org.springframework.boot.micrometer.tracing.opentelemetry.autoconfigure.otlp.OtlpTracingConnectionDetails;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.lang.Nullable;
+import org.springframework.test.context.DynamicPropertyRegistrar;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.utility.DockerImageName;
@@ -30,4 +34,23 @@ public class ContainersConfig {
                 + jaegerContainer.getMappedPort(4318) + "/v1/traces";
     }
 
+    @Bean
+    MicrocksContainer microcks(Network network) {
+        return new MicrocksContainer("quay.io/microcks/microcks-uber:1.13.2-native")
+            .withNetwork(network)
+            .withMainArtifacts("anthropic-openapi.yaml")
+            .withSecondaryArtifacts("anthropic-metadata.yaml", "anthropic-examples.yaml")
+            .withEnv("OTEL_EXPORTER_OTLP_ENDPOINT", "http://jaeger:4317")
+            .withEnv("OTEL_TRACES_EXPORTER", "otlp")
+            .withDebugLogLevel();
+    }
+
+    @Bean
+    public DynamicPropertyRegistrar properties(@Nullable MicrocksContainer microcks) {
+        return (registrar) -> {
+            if (microcks != null) {
+                registrar.add("spring.ai.anthropic.base-url", () -> microcks.getRestMockEndpoint("Anthropic API", "0.83.0"));
+            }
+        };
+    }
 }
