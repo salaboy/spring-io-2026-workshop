8c5386fac945d69da400f8b2b7a1f8f16a1bf448
Laurent Broudoux <laurent.broudoux@gmail.com>
2026-04-14 12:17:48 +0200

    Adding otel everywhere and removing code to complete

--- step-01/store/src/test/java/com/example/store/step01/StoreTests.java
+++ step-01/store/src/test/java/com/example/store/step01/StoreTests.java
@@ -28,27 +28,7 @@ public class StoreTests {
 
     @Test
     void testSpringAIChatMockTemplate()  {
-        // Call the /api/chat endpoint with a message asking to list all products in the inventory, and check that the response contains the expected product information
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        HttpEntity<String> requestEntity = new HttpEntity<>("""
-                {
-                    "conversationId": "abc",
-                    "message": "List all the products in the inventory"
-                }
-            """, headers);
-
-        ResponseEntity<String> response = restTemplate.postForEntity("/api/chat", requestEntity, String.class);
-
-        String body = response.getBody();
-
-        // Assert that the response contains the expected product information.
-        // This is coming from the Microcks mocks response.
-        assertNotNull(body);
-        assertTrue(body.contains("**Spring Boot**- T-Shirt: 50 units @ $29.99"));
-        assertTrue(body.contains("Socks: 100 units @ $12.99"));
-        assertTrue(body.contains("Sticker: 200 units @ $4.99"));
-        assertTrue(body.contains("**Spring AI**- T-Shirt: 30 units @ $29.99"));
+        // Call the /api/chat endpoint with a message asking to list all products in 
+        // the inventory, and check that the response contains the expected product information.
     }
 }
